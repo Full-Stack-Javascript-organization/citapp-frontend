99122c9c94446c141ce875aad03df8583f09fb7f
brero12 <8272332+brero12@users.noreply.github.com>
2023-08-08 23:13:30 -0700

    Adding conection between landing, login and homepage in frontend, notification service on backend

---
+++ citas-backend/src/config/mail.js
@@ -0,0 +1,9 @@
+
+
+module.exports = {
+  mailgun: {
+    USERNAME: process.env.MAILGUN_USERNAME,
+    DOMAIN: process.env.MAILGUN_DOMAIN,
+    API_KEY: process.env.MAILGUN_API_KEY,
+  }
+}
--- citas-backend/src/index.js
+++ citas-backend/src/index.js
@@ -5,6 +5,8 @@ const companyRoutes = require('./routes/company');
 const serviceRoutes = require('./routes/service');
 const calendarRoutes = require('./routes/calendar');
 const cors = require('cors');
+const NotificationService = require('./app/services/notificationService');
+
 
 //const bodyParser = require('body-parser');
 
@@ -18,16 +20,30 @@ app.use(express.json()) // To parse the incoming requests with JSON payloads
 
 connectDB();
 
-app.use(express.json());
+/*app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/company', companyRoutes);
 app.use('/service', serviceRoutes);
-app.use('/calendar', calendarRoutes);
+app.use('/calendar', calendarRoutes);*/
 
 
 
 app.get('/', (req,res) => {
     res.send(`Hello There`); 
+})
+
+app.get('/notify', async (req,res) => {
+
+  const notification = new NotificationService();
+
+  const toEmail = 'brero12@gmail.com'
+
+  console.log('toEmail' + toEmail);
+
+  await notification.sendNotification(toEmail,{
+    subject: `CitApp Notification Services`,
+    text: `This is a test for citapp notification service.`
+  });
 })
 
 app.listen(3001, () => {
