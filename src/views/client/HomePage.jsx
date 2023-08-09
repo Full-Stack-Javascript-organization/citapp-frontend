fb1ef1865e8bbb8f0fc57d081c05f3494f7ff41d
brero12 <8272332+brero12@users.noreply.github.com>
2023-08-09 01:53:05 -0700

    Sending hardcore company data from homepage to reservations

--- src/views/client/HomePage.jsx
+++ src/views/client/HomePage.jsx
@@ -2,16 +2,43 @@ import React from "react";
 import Card from "./StoreCard";
 
 export function HomePageBody() {
+  const companies = [
+    {
+      companyName: "ABC",
+      companyID: 1,
+      address: "Address 123",
+      availableTime: "9:00 am to 5:00pm",
+      status: "active",
+    },
+    {
+      companyName: "Company 2",
+      companyID: 2,
+      address: "Address 345",
+      availableTime: "9:00 am to 5:00pm",
+      status: "active",
+    },
+    {
+      companyName: "Company 3",
+      companyID: 3,
+      address: "Hilton",
+      availableTime: "9:00 am to 5:00pm",
+      status: "active",
+    },
+  ];
+  
+
   return (
     <div className=" w-full grid grid-cols-4 p-2 desktop:grid-cols-4 tablet:grid-cols-2 laptop:grid-cols-3 gap-4 mt-5">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      
+      {companies.map((company) =>  {
+        return (
+         
+        <Card key={company.companyID} company={company}/>
+         
+        );
+      }
+      )}
+           
     </div>
   );
 }
